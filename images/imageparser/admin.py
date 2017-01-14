75cf7bcd47f38329e1b298a828ac3769d58a68c5
astashkevich <a.stashkevich@ip.net.ua>
2017-01-14 08:51:54 +0200

    add image preview

--- images/imageparser/admin.py
+++ images/imageparser/admin.py
@@ -7,13 +7,13 @@ from .models import  Site, Photo
 class PhotoInline(admin.TabularInline):
 	model = Photo
 	extra = 0
-	#exclude = ('image_thumbnail',)
-	#readonly_fields = ('get_photo', 'date_modified')
+	exclude = ('image_thumbnail',)
+	readonly_fields = ('get_photo', 'date_modified')
 
-	# def get_photo(self, obj):
-	# 	return u'<img src="%s" width="100" />' % obj.image_thumbnail.url
-	#get_photo.short_description = 'Preview'
-	#get_photo.allow_tags = True
+	def get_photo(self, obj):
+		return u'<img src="%s" width="100" />' % obj.image_thumbnail.url
+	get_photo.short_description = 'Preview'
+	get_photo.allow_tags = True
 
 
 
